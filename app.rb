a030fd94a074b33c3cb1a67b7e31fb500d7f8b50
Bobby Neal <rsneal01@gmail.com>
2020-12-29 18:38:18 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,7 +19,7 @@ class App < Sinatra::Base
     @shooting_guard = params[:sg]
     @small_forward = params[:sf]
     @power_forward = params[:pf]
-    @center = params[:center]
+    @center = params[:c]
     
     erb :team
   end
