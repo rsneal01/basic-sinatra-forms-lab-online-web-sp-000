4bf54299cc8117539510ade1652a81fe4ac6bf1a
Bobby Neal <rsneal01@gmail.com>
2020-12-29 18:38:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,10 +15,10 @@ class App < Sinatra::Base
   post '/newteam' do
     @team_name = params[:name]
     @coach = params[:coach]
-    @point_guard = params[:point_guard]
-    @shooting_guard = params[:shooting_guard]
-    @small_forward = params[:small_forward]
-    @power_forward = params[:power_forward]
+    @point_guard = params[:pg]
+    @shooting_guard = params[:sg]
+    @small_forward = params[:sf]
+    @power_forward = params[:pf]
     @center = params[:center]
     
     erb :team
